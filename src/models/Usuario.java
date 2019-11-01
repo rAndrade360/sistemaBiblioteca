d4ed7a816bcbeafae6cf9b2cdc900f069f2246a7
Renan de Andrade <r.andrade@acad.ifma.edu.br>
2019-11-01 08:54:32 -0300

    Criação do model Usuário

--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -10,5 +10,35 @@ package models;
  * @author renan
  */
 public class Usuario {
+    private String nome;
+    private String matricula;
+    private int cpf;
+    private String turma;
+    private String endereco;
+    private int telefone;
+    private int celular;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
     
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+    
+    public String getMatricula(){
+        return this.matricula;
+    }
+
+    public void setCpf(int cpf){
+        this.cpf = cpf;
+    }
+    
+    public String getCpf(){
+        return this.cpf;
+    }
 }
